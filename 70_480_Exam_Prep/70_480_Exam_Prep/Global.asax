5cb6cf34c0958a28c36d99d8f9305881198a6300
Rupom Ahsan <rupom@live.com>
2015-10-18 23:08:26 +0800

    Initial Projects with Drop Down Menus

---
+++ 70_480_Exam_Prep/70_480_Exam_Prep/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_70_480_Exam_Prep.MvcApplication" Language="C#" %>
---
+++ 70_480_Exam_Prep/70_480_Exam_Prep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_70_480_Exam_Prep.Startup))]
+namespace _70_480_Exam_Prep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
